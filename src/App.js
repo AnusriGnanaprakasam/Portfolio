d5a74526dd79ecf94fb64953d789dfac2aba5eea
admin <admin@admins-MacBook-Pro.local>
2024-10-18 00:20:55 -0700

    making a repo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
 function App() {
   return (
     <div className="App">
@@ -16,6 +23,10 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
         </a>
       </header>
     </div>
